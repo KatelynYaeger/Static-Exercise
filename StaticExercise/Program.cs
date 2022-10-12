eb9aa13de556ae16d362c6e005ce8ec9e65bf574
Katelyn Yaeger <knyaeger93@gmail.com>
2022-10-12 17:24:00 -0500

    initial commit

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -1,10 +1,28 @@
-﻿namespace StaticExercise
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Reflection.Metadata;
+using System.Xml.Linq;
+
+namespace StaticExercise
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var celsius = TempConverter.FahrenheitToCelsius(32);
+            Console.WriteLine($"Celsius {celsius}");
+
+            var fahrenheit = TempConverter.CelsiusToFahrenheit(0);
+            Console.WriteLine($"Fahrenheit {fahrenheit}");
         }
     }
 }
+
+//Create a static class called TempConverter - DONE
+//Create 2 methods inside the TempConverter class - DONE
+//Name the first method - FahrenheitToCelsius - DONE
+//Name the second method - CelsiusToFahrenheit - DONE
+//Both methods will have a single parameter of type double - DONE
+//Complete the logic so that they properly convert the temperatures - DONE
+//Call both methods in your Main() method - DONE
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+
+        public static double FahrenheitToCelsius(double fahrenheit)
+        {
+            return (fahrenheit - 32) / (1.8);
+        }
+
+        public static double CelsiusToFahrenheit(double celsius)
+        {
+            return (celsius* 9)/5 + 32;
+        }
+
+
+        //(32°F − 32) × 5/9 = 0°C
+        //(0°C × 9/5) + 32 = 32°F
+
+
+
+    }
+
+}
+
+//Create a static class called TempConverter - DONE
+//Create 2 methods inside the TempConverter class - DONE
+//Name the first method - FahrenheitToCelsius - DONE
+//Name the second method - CelsiusToFahrenheit - DONE
+//Both methods will have a single parameter of type double - DONE
+//Complete the logic so that they properly convert the temperatures
+//Call both methods in your Main() method
